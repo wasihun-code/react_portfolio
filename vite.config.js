8fc0b50b02f418b6abde0b10f0e4359e10824ec1
wasihun-code <wasihunageru7947@gmail.com>
2024-08-11 23:17:54 +0530

    Added jekyll file to fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react_portfolio/', // Ensure this matches your GitHub Pages path
   plugins: [react()],
-  base: "./"
-})
+});
