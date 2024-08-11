4d18010f9d85d256ee63fd3c5735a3f04eac1f45
wasihun-code <wasihunageru7947@gmail.com>
2024-08-12 00:33:14 +0530

    deployment issues

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react_portfolio/', // Ensure this matches your GitHub Pages path
   plugins: [react()],
 });
