c4211dcf3f89ac342430653eb612462a70060184
wasihun-code <wasihunageru7947@gmail.com>
2024-08-12 00:51:41 +0530

    Added jekyll file to fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "https://wasihun-code.github.io/react_portfolio/",
+  base: "./",
   plugins: [react()],
 });
