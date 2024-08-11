e0fd8b538b38b2995f4d0e0b6cc84ed5502a7f22
wasihun-code <wasihunageru7947@gmail.com>
2024-08-11 22:45:23 +0530

    Fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/react_portfolio/', 
   plugins: [react()],
+  base: "https://wasihun-code.github.io/react_portfolio/"
 })
